73d444795548d09c158755e06398f03de647c087
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-25 23:53:56 +0200

    first commit: start index html page and java entities

---
+++ src/main/java/com/rafaelli/my_vocabulary/MyVocabularyApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelli.my_vocabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVocabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVocabularyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelli/my_vocabulary/MyVocabularyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelli.my_vocabulary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVocabularyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
